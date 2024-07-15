c84b0f3c6305fb98c812fa147b123e34335b6f94
AmitNGH <Amit963n@gmail.com>
2024-07-15 17:34:58 +0300

    Added basic functionality for each method type

--- webapp/rest_app.py
+++ webapp/rest_app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from datetime import datetime
 from db_handler import db_connection
 
 app = Flask(__name__)
@@ -11,12 +12,14 @@ def get_user(user_id):
     response = {}
 
     with db_connection().cursor() as cursor:
-        cursor.execute(f"SELECT user_name FROM `users` WHERE `user_id`={user_id}")
+        cursor.execute(f"SELECT user_name "
+                       f"FROM `users` "
+                       f"WHERE `user_id`={user_id}")
         if cursor.rowcount == 0:
             response["status"] = "error"
             response["reason"] = "no such id"
             return_code = 500
-        else :
+        else:
             response["status"] = "ok"
             response["user_name"] = cursor.fetchone()
             return_code = 200
@@ -26,17 +29,37 @@ def get_user(user_id):
 
 @app.route(backend_route, methods=['POST'])
 def create_user(user_id):
-    print("Hello World")
+    request_payload = request.json()
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    user_name = request_payload['user_name']
+
+    # TODO: check if user with same id exists, Error handling
+
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"INSERT INTO users (user_id, user_name, creation_date) "
+                       f"VALUES ({user_id}, {user_name}, {now}")
 
 
 @app.route(backend_route, methods=['PUT'])
 def update_user(user_id):
-    print("Hello World")
+    request_payload = request.json()
+    user_name = request_payload['user_name']
+
+    # TODO: Check if user exists before updating, Error handling
+
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"UPDATE users "
+                       f"SET user_name = {user_name} "
+                       f"WHERE user_id = {user_id}")
 
 
 @app.route(backend_route, methods=['DELETE'])
 def remove_user(user_id):
-    print("Hello World")
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"DELETE FROM users"
+                       f"WHERE user_id = {user_id}")
+
+        # TODO: Check if user exists before deleting, Error handling
 
 
 def run_rest_app():
