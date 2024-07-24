4badcb3792819ed5391e495259b69955f0bf0160
AmitNGH <amit963n@gmail.com>
2024-07-24 12:56:28 +0300

    Finished implementing PUT method

--- webapp/rest_app.py
+++ webapp/rest_app.py
@@ -1,10 +1,9 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
 
-from werkzeug.exceptions import UnsupportedMediaType
-
 from db_handler import db_connection
-from Utils import (ok_response_template,
+from Utils import (extract_json_from_request,
+                   ok_response_template,
                    unprocessable_entity_response_template,
                    unsupported_media_type_response_template,
                    internal_server_error_response_template,
@@ -38,6 +37,7 @@ def get_user(user_id):
 @app.route(backend_route, methods=['POST'])
 def create_user(user_id):
 
+    # TODO: Extract duplicate code to method, use in POST, PUT
     is_right_format, request_payload = extract_json_from_request(request)
 
     # Returns error in-case request is not in json format
@@ -47,6 +47,7 @@ def create_user(user_id):
 
         return jsonify(response), return_code
 
+    # TODO: Extract duplicate code to method, use in POST, PUT
     # Returns error is user_name key does not exist
     user_name = request_payload.get("user_name")
 
@@ -79,30 +80,42 @@ def create_user(user_id):
 
 @app.route(backend_route, methods=['PUT'])
 def update_user(user_id):
-    # Get payload from request
-    request_payload = request.get_json()
 
-    # Prepare data for query
-    user_name = request_payload['user_name']
+    # TODO: Extract duplicate code to method, use in POST, PUT
+    is_right_format, request_payload = extract_json_from_request(request)
 
-    response = {}
+    if not is_right_format:
+        response, return_code = unsupported_media_type_response_template()
+        response["reason"] = "payload should be json"
+
+        return jsonify(response), return_code
+
+    # TODO: Extract duplicate code to method, use in POST, PUT
+    # Returns error is user_name key does not exist
+    user_name = request_payload.get("user_name")
+
+    if not user_name:
+        response, return_code = unprocessable_entity_response_template()
+        response["reason"] = "json does not contain user_name"
+
+        return jsonify(response), return_code
 
     with db_connection().cursor() as cursor:
-        cursor.execute(f"UPDATE users "
-                       f"SET user_name = '{user_name}' "
-                       f"WHERE user_id = {user_id}")
+        id_exists, user_data = check_user_exists_by_id(user_id, cursor)
 
-        # TODO: Add id exists check, and add it to previous methods
-        # In case the user does not exist
-        if cursor.rowcount == 0:
-            response["status"] = "error"
-            response["reason"] = "no such id"
-            return_code = 500
-        else:
+        if id_exists:
+            cursor.execute(f"UPDATE users "
+                           f"SET user_name = '{user_name}' "
+                           f"WHERE user_id = {user_id}")
             db_connection().commit()
-            response["status"] = "ok"
-            response["user_updated"] = cursor.fetchone()[0]
-            return_code = 200
+
+        # In case the user does not exist
+        if id_exists:
+            response, return_code = ok_response_template()
+            response["user_updated"] = user_name
+        else:
+            response, return_code = internal_server_error_response_template()
+            response["reason"] = "no such id"
 
     return jsonify(response), return_code
 
@@ -116,8 +129,10 @@ def remove_user(user_id):
         # TODO: Check if user exists before deleting, Error handling
 
 
-#
+# Runs a SELECT query on DB to check if the user_id exists
+# Returns tuple containing if the user_id exists and the user object in-case it does
 def check_user_exists_by_id(user_id, cursor) -> tuple[bool, dict]:
+    db_connection().commit()
     cursor.execute(f"SELECT user_id, user_name, creation_date "
                    f"FROM users "
                    f"WHERE user_id={user_id}")
@@ -126,17 +141,6 @@ def check_user_exists_by_id(user_id, cursor) -> tuple[bool, dict]:
         return True, cursor.fetchone()
 
     return False, {}
-
-
-# Attempts to extract json object from given request -
-# Returns tuple containing if the operation was successful and the extracted json object
-def extract_json_from_request(json_request) -> tuple[bool, dict]:
-    try:
-        request_json = json_request.get_json()
-    except UnsupportedMediaType:
-        return False, {}
-
-    return True, request_json
 
 
 def run_rest_app():
