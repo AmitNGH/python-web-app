5e0bd8b7ac60330b2633826b1e87b1f077ff75b3
AmitNGH <amit963n@gmail.com>
2024-07-26 20:21:08 +0300

    Moved check_user_exists_by_id to db_handler.py

--- webapp/db_handler.py
+++ webapp/db_handler.py
@@ -14,6 +14,23 @@ connection = pymysql.connect(host=config['DB']['host'],
                              password=config['DB']['password'])
 
 
+# Runs a SELECT query on DB to check if the user_id exists
+# Returns tuple containing if the user_id exists and the user object in-case requested
+def check_user_exists_by_id(user_id, cursor, return_user_object=False) -> bool | tuple[bool, dict]:
+    db_connection().commit()
+    if return_user_object:
+        cursor.execute(f"SELECT user_id, user_name, creation_date "
+                       f"FROM users "
+                       f"WHERE user_id={user_id}")
+    else:
+        cursor.execute(f"SELECT user_id "
+                       f"FROM users "
+                       f"WHERE user_id={user_id}")
+    if cursor.rowcount:
+        return True if not return_user_object else (True, cursor.fetchone())
+
+    return False if not return_user_object else (False, {})
+
 # Returns the db connection instance
 def db_connection():
     return connection
