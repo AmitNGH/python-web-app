f6b0488a715da7484a9cae53a3dd10e594a645a1
AmitNGH <amit963n@gmail.com>
2024-07-26 20:22:48 +0300

    Finished creating web interface

--- webapp/web_app.py
+++ webapp/web_app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from Utils import USER_NAME_INDEX_IN_DB
+from db_handler import (db_connection,
+                        check_user_exists_by_id)
 
 app = Flask(__name__)
 
 
-@app.route(endpoint="/users/get_user_data/<user_id>")
-def get_user(user_id):
-    print("Hello World")
+@app.route("/users/get_user_data/<user_id>")
+def get_user_name(user_id):
+    with db_connection().cursor() as cursor:
+        user_exists, user_object = check_user_exists_by_id(user_id, cursor, return_user_object=True)
+
+    if user_exists:
+        return "<H1 id='user'>" + user_object[USER_NAME_INDEX_IN_DB - 1] + "</H1>"
+    else:
+        return "<H1 id='error'>\" no such user: + user_id + \"</H1>"
 
 
 def run_web_app():
