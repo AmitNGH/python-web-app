59ed27b9d74db8c7814eae96a9a6f7c1f891a5ba
AmitNGH <Amit963n@gmail.com>
2024-07-29 20:22:18 +0300

    Added POST tests for backend_testing.py

--- webapp/test/backend_testing.py
+++ webapp/test/backend_testing.py
@@ -11,10 +11,22 @@ from webapp.Utils import (OK_RETURN_CODE,
                           INTERNAL_SERVER_ERROR_CODE)
 
 tests_user_id = -9999
-expected_ok_status = "ok"
-expected_error_status = "error"
 expected_user_name = "Amit"
+
+expected_ok_status = "ok"
 expected_ok_return_code = OK_RETURN_CODE
+
+expected_error_status = "error"
+expected_no_id_error_reason = "no such id"
+expected_internal_server_error_code = INTERNAL_SERVER_ERROR_CODE
+
+expected_id_exists_reason = "id already exists"
+
+expected_unsupported_format_reason = "payload should be json"
+expected_unsupported_media_type_code = UNSUPPORTED_MEDIA_TYPE_CODE
+
+expected_invalid_json_reason = "json does not contain user_name"
+expected_unprocessable_entity_code = UNPROCESSABLE_ENTITY_CODE
 
 
 def before_test_get_user_found():
@@ -41,9 +53,130 @@ def test_get_user_found():
         format_error_assertion_message("user_name", expected_user_name, actual_user_name))
 
 
+def before_test_get_user_not_found():
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"DELETE FROM users "
+                       f"WHERE user_id = {tests_user_id}")
+        db_connection().commit()
+
+
+def test_get_user_not_found():
+    json_response = request("GET", f"http://localhost:5000/users/{tests_user_id}")
+
+    actual_return_code = json_response.status_code
+
+    json_response = json_response.json()
+    actual_status = json_response["status"]
+    actual_reason = json_response["reason"]
+
+    assert expected_internal_server_error_code == actual_return_code, (
+        format_error_assertion_message("return_code", expected_internal_server_error_code, actual_return_code))
+    assert expected_error_status == actual_status, (
+        format_error_assertion_message("status", expected_error_status, actual_status))
+    assert expected_no_id_error_reason == actual_reason, (
+        format_error_assertion_message("user_name", expected_no_id_error_reason, actual_reason))
+
+
+def before_test_create_user_success():
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"DELETE FROM users "
+                       f"WHERE user_id = {tests_user_id}")
+        db_connection().commit()
+
+
+def test_create_user_success():
+    json_response = request("POST", f"http://localhost:5000/users/{tests_user_id}",
+                            json={"user_name": expected_user_name})
+
+    actual_return_code = json_response.status_code
+
+    json_response = json_response.json()
+    actual_status = json_response["status"]
+    actual_user_added = json_response["user_added"]
+
+    assert expected_ok_return_code == actual_return_code, (
+        format_error_assertion_message("return_code", expected_ok_return_code, actual_return_code))
+    assert expected_ok_status == actual_status, (
+        format_error_assertion_message("status", expected_ok_status, actual_status))
+    assert expected_user_name == actual_user_added, (
+        format_error_assertion_message("user_added", expected_user_name, actual_user_added))
+
+
+def before_test_create_user_already_exists():
+    with db_connection().cursor() as cursor:
+        cursor.execute(f"INSERT IGNORE INTO users (user_id, user_name, creation_date) "
+                       f"VALUES ({tests_user_id}, '{expected_user_name}', NULL)")
+        db_connection().commit()
+
+
+def test_create_user_already_exists():
+    json_response = request("POST", f"http://localhost:5000/users/{tests_user_id}",
+                            json={"user_name": expected_user_name})
+
+    actual_return_code = json_response.status_code
+
+    json_response = json_response.json()
+    actual_status = json_response["status"]
+    actual_reason = json_response["reason"]
+
+    assert expected_internal_server_error_code == actual_return_code, (
+        format_error_assertion_message("return_code", expected_internal_server_error_code, actual_return_code))
+    assert expected_error_status == actual_status, (
+        format_error_assertion_message("status", expected_error_status, actual_status))
+    assert expected_id_exists_reason == actual_reason, (
+        format_error_assertion_message("reason", expected_id_exists_reason, actual_reason))
+
+
+def test_create_user_unsupported_format():
+    json_response = request("POST", f"http://localhost:5000/users/{tests_user_id}",
+                            data={"user_name": expected_user_name})
+
+    actual_return_code = json_response.status_code
+
+    json_response = json_response.json()
+    actual_status = json_response["status"]
+    actual_reason = json_response["reason"]
+
+    assert expected_unsupported_media_type_code == actual_return_code, (
+        format_error_assertion_message("return_code", expected_unsupported_media_type_code, actual_return_code))
+    assert expected_error_status == actual_status, (
+        format_error_assertion_message("status", expected_error_status, actual_status))
+    assert expected_unsupported_format_reason == actual_reason, (
+        format_error_assertion_message("reason", expected_unsupported_format_reason, actual_reason))
+
+
+def test_create_user_invalid_json_format():
+    json_response = request("POST", f"http://localhost:5000/users/{tests_user_id}",
+                            json={"testField": expected_user_name})
+
+    actual_return_code = json_response.status_code
+
+    json_response = json_response.json()
+    actual_status = json_response["status"]
+    actual_reason = json_response["reason"]
+
+    assert expected_unprocessable_entity_code == actual_return_code, (
+        format_error_assertion_message("return_code", expected_unprocessable_entity_code, actual_return_code))
+    assert expected_error_status == actual_status, (
+        format_error_assertion_message("status", expected_error_status, actual_status))
+    assert expected_invalid_json_reason == actual_reason, (
+        format_error_assertion_message("reason", expected_invalid_json_reason, actual_reason))
+
+
 def run_tests():
+    # GET method tests
     before_test_get_user_found()
     test_get_user_found()
+    before_test_get_user_not_found()
+    test_get_user_not_found()
+
+    # POST method tests
+    before_test_create_user_success()
+    test_create_user_success()
+    before_test_create_user_already_exists()
+    test_create_user_already_exists()
+    test_create_user_unsupported_format()
+    test_create_user_invalid_json_format()
 
 
 if __name__ == '__main__':
